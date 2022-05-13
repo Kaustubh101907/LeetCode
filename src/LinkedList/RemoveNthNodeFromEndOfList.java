4b6444e08be9fab9af5df3e01fdb898d46a7df0e
Kaustubh101907 <kaustubhnaithani101010@gmail.com>
2022-05-13 22:05:00 +0530

    leetcode ans

--- src/LinkedList/RemoveNthNodeFromEndOfList.java
+++ src/LinkedList/RemoveNthNodeFromEndOfList.java
@@ -2,19 +2,6 @@ package LinkedList;
 
 // Que Link: https://leetcode.com/problems/remove-nth-node-from-end-of-list/
 
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) {
-        this.val = val;
-    }
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
-
 public class RemoveNthNodeFromEndOfList {
     public static void main(String[] args) {
 
