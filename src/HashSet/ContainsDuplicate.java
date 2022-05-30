7bc8a3d95f9e962bfec4b5b2c9b1e6ef5e870788
Kaustubh101907 <kaustubhnaithani101010@gmail.com>
2022-05-30 07:39:48 +0530

    leetcode ans

---
+++ src/HashSet/ContainsDuplicate.java
@@ -0,0 +1,22 @@
+package HashSet;
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1};
+        System.out.println(containsDuplicate(nums));
+    }
+
+    private static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> h = new HashSet<>();
+        for (int i = 0 ; i < nums.length; i++) {
+            if (!h.contains(nums[i])) {
+                h.add(nums[i]);
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/Strings/LongestCommonPrefix.java
@@ -0,0 +1,23 @@
+package Strings;
+
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        String[] strs = {"flower","flow","flight"};
+        System.out.println(longestCommonPrefix(strs));
+    }
+
+    private static String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() -1);
+                if (prefix.isEmpty())
+                    return "";
+            }
+        }
+        return prefix;
+    }
+}
