8f4a69a4e77af82aed59fc068f3bc22a80b34530
Kaustubh101907 <kaustubhnaithani101010@gmail.com>
2022-06-14 21:18:51 +0530

    leetcode ans

--- src/Strings/DeleteOperationForTwoString.java
+++ src/Strings/DeleteOperationForTwoString.java
@@ -1,37 +1,40 @@
 package Strings;
 
 // Que Link: https://leetcode.com/problems/delete-operation-for-two-strings/
+// Not Correct
 
 import java.util.HashSet;
 
 public class DeleteOperationForTwoString {
     public static void main(String[] args) {
-        String s1 = "sea";
-        String s2 = "eat";
+        String s1 = "leetcode";
+        String s2 = "etco";
         System.out.println(minDistance(s1, s2));
     }
 
     private static int minDistance(String s1, String s2) {
-//        String str1, str2;
-//        if (s1.length() > s2.length()) {
-//            str1 = s2;
-//            str2 = s1;
-//        } else {
-//            str1 = s1;
-//            str2 = s2;
-//        }
-        HashSet<String> h= new HashSet<>();
-        for (int i = 1; i <= s1.length(); i++) {
-            h.add(s1.substring(0,i));
+        String str1, str2;
+        if (s1.length() > s2.length()) {
+            str1 = s2;
+            str2 = s1;
+        } else {
+            str1 = s1;
+            str2 = s2;
         }
+        HashSet<String> h= new HashSet<>();
+        for (int i = str1.length(); i >= 0; i--) {
+            h.add(str1.substring(0,i));
+        }
+        System.out.println(h);
         int count = 0;
-        for (int i = s2.length() ; i <= 0; i--) {
-            if (h.contains(s2.substring(0,i))) {
-                return count;
+        for (int i = str2.length() ; i >= 0; i--) {
+            if (h.contains(str2.substring(0,i))) {
+                break;
             } else {
+                System.out.println(str2.substring(0,i));
                 count++;
             }
         }
-        return count;
+        return count -1;
     }
 }
